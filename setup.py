b2c7efdc18cc4f1fe636486cc99189af863d8871
Kaiser <34060097+Kaiz0r@users.noreply.github.com>
2019-12-31 13:34:48 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mogscript',
   packages = ['mogscript'],
-  version = '19.12.28.1',
+  version = '19.12.31',
   license='gpl-3.0',
   description = 'A sub-script language for quick text processing and automation.',
   author = 'Kaiser',
   author_email = 'technomancer@gmx.com',
   url = 'https://github.com/Kaiz0r/mogscript',
-  download_url = 'https://github.com/Kaiz0r/mogscript/archive/19.12.28.1.tar.gz',
+  download_url = 'https://github.com/Kaiz0r/mogscript/archive/19.12.31.tar.gz',
   keywords = ['subscript', 'markup', 'script'],
   classifiers=[
     # How mature is this project? Common values are
